5e786b7b71d9a863535a7c250aa28fcd5d38ab35
1mShafuu <74681526+1mShafuu@users.noreply.github.com>
2022-09-02 15:38:06 +0500

    1212

--- UI.Menu/Assets/Scripts/HealthUI.cs
+++ UI.Menu/Assets/Scripts/HealthUI.cs
@@ -1,37 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Slider), typeof(Player))]
 public class HealthUI : MonoBehaviour
 {
+    private const float СhangerSpeed = 9f;
+    
     [SerializeField] private Slider _slider;
     [SerializeField] private Player _player;
-    
-    private const float _changeSpeed = 5f;
 
     private float _changedSliderValue;
+    private Coroutine _healthChanger;
 
     private void Awake()
     {
          _changedSliderValue = _player.Health;
+         _slider.value = _player.Health;
     }
 
-    private void Update()
+    public void HealthChange()
     {
-        
-        _slider.value = Mathf.MoveTowards(_slider.value, _changedSliderValue,  _changeSpeed * Time.deltaTime);
-    }
+        if (_healthChanger != null)
+        {
+            StopCoroutine(_healthChanger);
+        }
 
-    public void Heal()
+        StartCoroutine(HealthChanger());
+    }
+    
+    private IEnumerator HealthChanger()
     {
+        const float seconds = 0.01f;
+        var waitForSeconds = new WaitForSeconds(seconds);
         _changedSliderValue = _player.Health;
-    }
 
-    public void Damage()
-    { 
-        _changedSliderValue = _player.Health;
+        while (_slider.value != _changedSliderValue)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, _changedSliderValue,  СhangerSpeed * Time.deltaTime);
+            yield return waitForSeconds;
+        }
     }
 }
--- UI.Menu/Assets/Scripts/Player.cs
+++ UI.Menu/Assets/Scripts/Player.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-   private const float _changeValue = 10;
-   private const float maxHealth = 100f;
-   private const float minHealth = 0f;
+   private const float ChangeValue = 10;
+   private const float MaxHealth = 100f;
+   private const float MinHealth = 0f;
+   
+   [SerializeField] private UnityEvent PlayerHealthChanged;
    
    private float _health;
 
@@ -19,17 +22,19 @@ public class Player : MonoBehaviour
 
    public void TakeDamage()
    {
-      if (_health > minHealth)
+      if (_health > MinHealth)
       {
-         _health -= _changeValue;
+         _health -= ChangeValue;
+         PlayerHealthChanged.Invoke();
       }
    }
 
    public void TakeHeal()
    {
-      if (_health < maxHealth)
+      if (_health < MaxHealth)
       {
-         _health += _changeValue;
+         _health += ChangeValue;
+         PlayerHealthChanged.Invoke();
       }
    }
 }
